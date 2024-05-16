d990e3c7eef3d646d49c08072189fcdfaac41211
jeje <jessa.andromeda@gmail.com>
2024-05-16 08:39:22 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/login', function () {
+    return view('login');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/infopesanan', function () {
+    return view('infopesanan');
 });
