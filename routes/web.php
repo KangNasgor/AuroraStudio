eb2d698177357c464bc0cac86075ec3b179a7d58
raditya <vani05435@gmail.com>
2024-05-20 11:10:29 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 Route::get('/example', function () {
     return view('example');
 });
+//loggin proses//
+Route::get('/sesi', [SessionController::class, 'index'])->name('sesi');
+Route::post('/sesi/login', [SessionController::class, 'login'])->name('login.proses');
+//register proses//
+Route::get('/registrasi', [SessionControllerController::class, 'registrasi'])->name('registrasi');
+Route::post('/registrasi.proses', [SessionControllerController::class, 'registrasi_proses'])->name('registrasi.proses');
