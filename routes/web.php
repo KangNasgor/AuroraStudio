1fa28d439d24e4bba1a0b1191ac81814aeae2e95
ara <aqeela.ara19@gmail.com>
2024-05-20 11:15:36 +0700

    booking

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/booking', function () {
+    return view('booking');
+});
+
+Route::get('/account', function () {
+    return view('account');
 });
