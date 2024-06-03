f074a3ef3d6c5de52d46b7c539415cd61a75997e
jeje <jessa.andromeda@gmail.com>
2024-06-03 14:49:10 +0700

    jj

--- resources/views/infopesanan.blade.php
+++ resources/views/infopesanan.blade.php
@@ -9,21 +9,19 @@
     }
 
     .tableinformation {
-    background-color: #4683b4a9;
-    backdrop-filter: blur(5px); /* Efek blur hanya diterapkan pada latar belakang tabel */
-    border: 2px solid black;
-}
+        background-color: #4683b4a9;
+        backdrop-filter: blur(5px); /* Efek blur hanya diterapkan pada latar belakang tabel */
+        border: 2px solid black;
+    }
 
-    .tableinformation th, td {
+    .tableinformation th, .tableinformation td {
         color: white;
         border: 2px solid black;
         background-color: #4683b4a9;
-        padding: 25px; /* Mengurangi padding agar tabel lebih kecil */ 
+        padding: 25px; /* Mengurangi padding agar tabel lebih kecil */
     }
 
     .tableinformation td {
-        color: white;
-        background-color: #4683b4a9;    
         border-bottom: 2px solid black; /* Mengatur garis bawah tebal untuk elemen td */
     }
 
@@ -41,11 +39,10 @@
     .card {
         background-color: #6494ed84;
         border-radius: 15px;
-        box-shadow: 0 0 3   5px rgba(0, 0, 0, 0.2);
+        box-shadow: 0 0 35px rgba(0, 0, 0, 0.2); /* Memperbaiki nilai box-shadow */
     }
-
-    
 </style>
+
 <section id="hero" class="container">
     <!--text infopesanan-->
     <div class="textinfopesanan">
@@ -54,7 +51,8 @@
     <!--bagian dalam infopesanan-->
     <div class="row justify-content-center">
         <div class="card text-center col-md-8" style="width: 50rem;">
-            <div class="container mt-3 ">
+            <div class="container mt-3">
+                @if($bookings)
                 <table class="table tableinformation">
                     <thead>
                         <tr>
@@ -93,10 +91,14 @@
                         </tr>
                     </tbody>
                 </table>
+                @else
+                <p class="text-center">Anda belum melakukan booking.</p>
+                @endif
             </div>
         </div>
     </div>
 </section>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 @endsection
