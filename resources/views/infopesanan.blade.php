b07b3899ef1e3a2a41e0032a487749b60f84620c
ara <aqeela.ara19@gmail.com>
2024-06-10 17:35:39 +0700

    ok mntap

--- resources/views/infopesanan.blade.php
+++ resources/views/infopesanan.blade.php
@@ -18,7 +18,7 @@
         color: white;
         border: 2px solid black;
         background-color: #4683b4a9;
-        padding: 25px;
+        padding: 15px;
     }
 
     .tableinformation td {
@@ -28,7 +28,7 @@
     .tableinformation th:first-child,
     .tableinformation td:first-child {
         width: 30%;
-        padding: 20px;
+        padding: 10px;
     }
 
     .tableinformation th:last-child,
@@ -43,59 +43,61 @@
     }
 </style>
 
-<section id="hero" class="container">
+<section id="hero" class="container my-4 px-4">
     <div class="textinfopesanan">
         <h1>INFO PESANAN</h1>
     </div>
     <div class="row justify-content-center">
-        <div class="card text-center col-md-8 mb-4" style="width: 50rem;">
+        <div class="card text-center col-md-8 mb-4">
             <div class="container mt-3">
                 @auth
                     @if($bookings)
-                        <table class="table tableinformation">
-                            <thead>
-                                <tr>
-                                    <th>Information</th>
-                                    <th>Details</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Nama</td>
-                                    <td>{{ $bookings->name }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Paket</td>
-                                    <td>{{ $bookings->paket }}</td>
-                                </tr>
-                                <tr>
-                                    <td>No Telepon</td>
-                                    <td>{{ $bookings->phone }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td>{{ $bookings->email }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Lokasi</td>
-                                    <td>
-                                        @if($bookings->tempat === 'Indoor' && empty($bookings->lokasi))
-                                            Studio Aurora
-                                        @else
-                                            {{ $bookings->lokasi }}
-                                        @endif
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Hari dan Tanggal</td>
-                                    <td>{{ $bookings->booking_date }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Waktu</td>
-                                    <td>{{ $bookings->jam }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="table-responsive">
+                            <table class="table tableinformation">
+                                <thead>
+                                    <tr>
+                                        <th>Information</th>
+                                        <th>Details</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Nama</td>
+                                        <td>{{ $bookings->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Paket</td>
+                                        <td>{{ $bookings->paket }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>No Telepon</td>
+                                        <td>{{ $bookings->phone }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Email</td>
+                                        <td>{{ $bookings->email }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lokasi</td>
+                                        <td>
+                                            @if($bookings->tempat === 'Indoor' && empty($bookings->lokasi))
+                                                Studio Aurora
+                                            @else
+                                                {{ $bookings->lokasi }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Hari dan Tanggal</td>
+                                        <td>{{ $bookings->booking_date }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Waktu</td>
+                                        <td>{{ $bookings->jam }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     @else
                         <p class="text-center">Anda belum melakukan booking.</p>
                     @endif
